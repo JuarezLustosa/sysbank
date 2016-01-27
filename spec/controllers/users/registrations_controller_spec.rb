e0692269a6acec38ecf661b9a27515e8a3bfa6a9
Juarez Lustosa <juarez.lustosa@gmail.com>
2016-01-26 23:05:47 -0200

    First test to controller

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Users::RegistrationsController, type: :controller do
-
+	describe 'user access' do
+    before :each do
+      @user = FactoryGirl.create(:user)
+      session[:user_id] = @user.id   
+    end
+  end
 end
+
