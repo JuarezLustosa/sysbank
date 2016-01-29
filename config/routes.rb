933c6f781a9569cfe57e86133e015276655bf058
Juarez Lustosa <juarez.lustosa@gmail.com>
2016-01-29 14:49:42 -0200

    routes and menus adds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :transactions, only: :index
+
+  namespace :transactions do
+    resource :deposit,  controller: 'deposit',   only: [:create, :new]
+    resource :withdraw, controller: 'withdraw',  only: [:create, :new]
+    resource :transfer, controller: 'transfer',  only: [:create, :new]
+  end
+
   resources :accounts do
     resource :close, controller: 'accounts/close',  only: :update
   end
