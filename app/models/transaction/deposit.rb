e0b17ded13f8af68968168d1511fffd46005d6ee
Juarez Lustosa <juarez.lustosa@gmail.com>
2016-01-29 01:04:00 -0200

    class to deposit

---
+++ app/models/transaction/deposit.rb
@@ -0,0 +1,49 @@
+ValidationError = Class.new RuntimeError
+class Transaction
+  class Deposit
+    include Virtus.model
+
+    include ActiveModel::Model
+
+    attr_reader :account, :ammount, :location
+
+    attribute :ammount, Decimal
+    attribute :account, Integer
+    attribute :location, String
+    attribute :action, String, :default => "deposit"
+
+    validates :ammount, :numericality => { :greater_than =>  0 }, :presence => true
+    validates :account, :presence => true
+    validate  :account_notfound
+
+    def persisted?
+      false
+    end
+
+    def find_account
+      Account.find_by_id(account)
+    end
+
+    def account_notfound
+      errors.add(:account, "Conta não encontrada") unless find_account
+    end
+
+    def save
+      if valid?
+        persist!
+        true
+      else
+        false
+      end
+    end
+
+  private
+    def persist!
+      @transaction = Transaction.create!(
+        account: find_account,
+        ammount: ammount,
+        action: action,
+        location: location)
+    end
+  end
+end
---
+++ spec/models/transaction/deposit_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe Transaction::Deposit do
+  let(:ammount) {100.90}
+  let(:params){ {"account"=> account, "ammount"=> ammount} }
+  let(:params_negative_ammount){ {"account"=> account, "ammount"=> negative_ammount} }
+  let(:account) { Account.new }
+  let(:negative_ammount) { -100 }
+  let(:deposit) { Transaction::Deposit.new(params) }
+
+  before do
+    allow(deposit).to receive(:find_account).and_return (account)
+  end
+
+  context "transaction instance" do
+    it "save" do
+      expect(deposit.save).to be_truthy
+    end
+  end
+
+  context "ammount" do
+    it "must be greater than 0" do
+      expect(Transaction::Deposit.new(params_negative_ammount).save).to be_falsey
+    end
+  end
+
+  context "validate presence of" do
+    it "account" do
+      expect(Transaction::Deposit.new({"account"=> nil, "ammount"=> ammount}).save).to be_falsey
+    end
+  end
+
+  context "account not found" do
+    before do allow(deposit).to receive(:find_account).and_return (nil) end
+
+    it "return error" do
+      expect(deposit.save).to be_falsey
+    end
+
+    it "content a error" do
+      deposit.save
+      expect(deposit.errors[:account]).to eq (["Conta não encontrada"])
+    end
+  end
+
+  context "action" do
+    it "must save a string deposit" do
+      expect(deposit.action).to eq("deposit")
+    end
+  end
+end
